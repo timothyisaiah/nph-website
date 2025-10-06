a7a51ca5bb05816cc68a1f9b1a6fb1067a13ced9
timothyisaiah <timothyisaiah7@gmail.com>
2025-10-06 14:52:46 +0300

    SVG updates

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1024,8 +1024,8 @@ const Home: React.FC = () => {
                           <div className="home-arc-description whitespace-nowrap">{indicator.shortDescription}</div>
                         </div>
                         <div className="home-arc-arrow flex-shrink-0">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
                         </svg>
                         </div>
                       </motion.div>
@@ -1370,8 +1370,8 @@ const Home: React.FC = () => {
                   <div className="home-tablet-description">{indicator.shortDescription}</div>
                 </div>
                 <div className="home-tablet-arrow">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
                         </svg>
                 </div>
               </motion.div>
@@ -1407,8 +1407,8 @@ const Home: React.FC = () => {
                     <div className="home-tablet-description">{indicator.shortDescription}</div>
                   </div>
                   <div className="home-tablet-arrow">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
                     </svg>
                   </div>
                 </motion.div>
